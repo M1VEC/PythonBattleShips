9e0176628b7697409d9be84a9f5848aa30989293
Philip Bilton <pbilton85@gmail.com>
2021-02-09 10:50:53 +0000

    game cleaned up, detects previous hit or miss

--- game.py
+++ game.py
@@ -5,37 +5,38 @@ class game:
         self.boardSize = board.getBoardSize()
         self.userBoard = board.getUserBoard()
         self.gameBoard = self.boats.getGameBoard()
-        self.userGuessX = userGuess[0]
-        self.userGuessY = userGuess[1]
+        self.userGuess = userGuess
+        self.shot = 0
         return self.runGame()
 
     def runGame(self):
-        print(self.getGameBoardContents(self.userGuessX, self.userGuessY))
-        if self.getGameBoardContents(self.userGuessX, self.userGuessY) == 'B ':
-            print("HIT")
-        elif self.getGameBoardContents(self.userGuessX, self.userGuessY) == 'M ':
-            print("Already tried")
-        elif self.getGameBoardContents(self.userGuessX, self.userGuessY) == 'H ':
-            print("Already hit")    
-        else:
-            print("MISS")
-        return
-
-    def checkShot(self):
-        return
+        self.shot = self.getGameBoardContents(self.userGuess)
+        if self.shot == 'B ':
+           return self.hit()
+        elif self.shot == '..':
+            return self.miss()
+        return self.noHitOrMiss()
 
     def hit(self):
+        print("HIT")
         return
 
     def miss(self):
+        print("MISS")
         return
 
+    def noHitOrMiss(self):
+        if self.shot == 'H ':
+            return "Previous Hit"
+        elif self.shot == ' ':
+            return "Previous Miss" 
+        return
 
-    def getUserBoardContents(self, AxisX, AxisY):
-        return self.userBoard[AxisX, AxisY]
+    def getUserBoardContents(self,coOrdinates):
+        return self.userBoard[coOrdinates[0], coOrdinates[1]]
 
-    def getGameBoardContents(self, AxisX, AxisY):
-        return self.gameBoard[AxisX, AxisY]    
+    def getGameBoardContents(self, coOrdinates):
+        return self.gameBoard[coOrdinates[0], coOrdinates[1]]
 
     def setUserBoard(self, coOrdinates, prompt):
         self.userBoard[coOrdinates[0], coOrdinates[1]] = '{:2}'.format(prompt)
